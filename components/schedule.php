a7cb9023c4f481d1ef8dc72920b26411b230c86a
AApalafox <adrian1144palafoxs@gmail.com>
2021-11-15 15:42:15 +0800

    Update schedule.php

--- components/schedule.php
+++ components/schedule.php
@@ -3,22 +3,22 @@
 
 	<div class="bg-primary col p-4">
 		<div class="card-body p-3 text-center">
-			<h5 class="brand text-light">
+			<h5 class="brand text-light fw-normal">
 				<i class="fas fa-ambulance fa-2x text-light"></i>
 				Emergency Hotline
 			</h5>
 			<p class="text-light">In case of emergency call the following number</p>
-			<p class="brand text-light">+63 45 125 0117</p>
+			<p class="brand text-light fw-normal">+63 45 125 0117</p>
 		</div>
 	</div>
 
 	<div class="bg-primary col p-4">
 		<div class="card-body">
-			<h5 class="brand text-light">
+			<h5 class="brand text-light fw-normal">
 				<i class="fas fa-clock fa-1x text-light"></i>
 				Opening Hours
 			</h5>
-			<p class="caption-body text-light">Our hospital is open for all during these hours</p>
+			<p class="text-light">Our hospital is open for all during these hours</p>
 		</div>
 		<ul class="list-group list-group-flush mb-3">
 			<a href="#">
