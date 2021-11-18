06defa437a5a43c84a676b43d64ab968f4338ee2
JPysus <72327740+JPysus@users.noreply.github.com>
2021-11-18 15:55:06 +0800

    added view, edit, delete on consultant user list

--- admin/user-list.php
+++ admin/user-list.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php 
+	include("endpoints/db.php");
+	$sql = "SELECT * FROM consultant";
+	$result = $conn->query($sql);
+
+	$ajaxVar = ["consultant_id", "fname", "lname", "email", "password", "job"];
+	$placeholder = ["ID", "First Name", "Last Name", "email", "password", "job"];
+?>
+	
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -24,76 +33,40 @@
 		<!-- page content -->
 		<div class="container">
 			<label class="caption-body">User List</label>
-			<table class="table table-hover  align-middle">
+			<!-- Display the list below -->
+			<!-- ung ginawa mo adrean <div id="displayConsultant"></div>-->
+			
+			
+			<!-- myTable -->
+			<table class="table table-hover align-middle" id="myTable">
 				<thead>
 					<tr>
-						<th scope="col">
-							ID
-							<button type="button" class="btn">
-								<i class="fas fa-arrow-up"></i>
-							</button>
-							<button type="button" class="btn">
-								<i class="fas fa-arrow-down"></i>
-							</button>
-						</th>
-						<th scope="col">Name</th>
-						<th scope="col">Username</th>
-						<th scope="col">
-							Action
-						</th>
+						<th scope="col">ID</th>
+						<?php 
+							for($i = 1; $i <count($placeholder); $i++)
+								echo '<th>',$placeholder[$i],'</th>';
+							echo '<td>Edit/Delete</td>';
+						?>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<th scope="row">1</th>
-						<td>Mark Otto</td>
-						<td>markOtto</td>
+	                <?php
+				while($row = $result->fetch_assoc()) {
+					echo '<tr>';
+					for($i = 0; $i <count($ajaxVar); $i++)
+						echo '<td>',$row[$ajaxVar[$i]],'</td>';
+					echo '
 						<td>
-							<button type="button" class="btn">
-								<i class="fas fa-eye"></i>
-							</button>
-							<button type="button" class="btn">
-								<i class="fas fa-edit"></i>
-							</button>
-							<button type="button" class="btn">
-								<i class="fas fa-trash-alt"></i>
-							</button>
+							<a class="del far fa-trash-alt" href="#" value="',$row[$ajaxVar[0]],'"/>
+							<a class="chg far fa-edit px-3" href="#" value="',$row[$ajaxVar[0]],'"/>
+							<a class="upd far fa-check-square" href="#" value="',$row[$ajaxVar[0]],'"style="display:none"/>
 						</td>
-					</tr>
-					<tr>
-						<th scope="row">1</th>
-						<td>Mark Otto</td>
-						<td>markOtto</td>
-						<td>
-							<button type="button" class="btn">
-								<i class="fas fa-eye"></i>
-							</button>
-							<button type="button" class="btn">
-								<i class="fas fa-edit"></i>
-							</button>
-							<button type="button" class="btn">
-								<i class="fas fa-trash-alt"></i>
-							</button>
-						</td>
-					</tr>
-					<tr>
-						<th scope="row">1</th>
-						<td>Mark Otto</td>
-						<td>markOtto</td>
-						<td>
-							<button type="button" class="btn">
-								<i class="fas fa-eye"></i>
-							</button>
-							<button type="button" class="btn">
-								<i class="fas fa-edit"></i>
-							</button>
-							<button type="button" class="btn">
-								<i class="fas fa-trash-alt"></i>
-							</button>
-						</td>
-					</tr>
+						';
+					echo '</tr>';
+				}
+	                ?>
 				</tbody>
-			</table>
+			</table><!-- end of myTable-->
 		</div>
 		<hr>
 		<?php include 'templates/bottom-nav.php'; ?>
@@ -101,3 +74,99 @@
 </body>
 
 </html>
+
+<script>
+	/** ung ginawa mo adrean
+	$(document).ready(displayConsultantList());
+
+	function displayConsultantList() {
+		var str = "all"
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				document.getElementById("displayConsultant").innerHTML = this.responseText;
+			}
+		};
+		xmlhttp.open("GET", "handlers/displayConsultantHandler.php?q=" + str, true);
+		xmlhttp.send();
+	}
+	*/
+	
+	//mine's below
+	$(document).ready(function () {
+	    $('#myTable').dataTable();
+	});
+	
+	//delete function per Table Row
+		$('.del').click(function(){
+			$(this).parent().parent().hide();
+			deleteRecord($(this).attr("value"));
+		});
+	//change function per Table Row
+		$('.chg').click(function(){
+			<?php 
+				for($i = 1; $i < count($ajaxVar); $i++)
+					echo '$(this).parent().parent().find(\'td:eq(',$i,')\').html("<input>");';
+			?>
+			$(this).hide();
+			$(this).parent().find('a:eq(0)').hide();
+			$(this).parent().find('a:eq(2)').show();
+		});
+
+	//update function per Table Row
+	$('.upd').click(function(){
+		updateRecord($(this).parent().parent());
+	});
+	//UPDATE
+		function updateRecord(e){
+			$.ajax({
+				'url':"endpoints/consultant/consultantUpdate.php",
+				'type':"POST",
+				'data':{
+					<?=$ajaxVar[0]?>: $(e).find('td:eq(0)').text(),
+					<?php 
+						for($i = 1; $i <count($ajaxVar); $i++)
+							echo $ajaxVar[$i],': $(e).find(\'td:eq(',$i,')\').find(\'input\').val(),';
+					?>
+				},
+				success: function(response){
+					response = JSON.parse(response);
+					if(response.code == 200){
+
+						window.location.reload()
+					}
+					else if (response.code == 400){
+						console.log(response.error);
+
+					}
+					else{
+						console.log(response.code);
+					}
+				}
+			});
+		}
+	//DELETE
+		function deleteRecord(record){
+			$.ajax({
+				'url':"endpoints/consultant/consultantDelete.php",
+				'type':"POST",
+				'data':{
+					<?=$ajaxVar[0]?>: record,
+				},
+				success: function(response){
+					response = JSON.parse(response);
+					if(response.code == 200){
+						console.log(response.code);
+					}
+					else if (response.code == 400){
+						console.log(response.error);
+
+					}
+					else{
+						console.log(response.code);
+					}
+				}
+			});
+		}
+	
+</script>
