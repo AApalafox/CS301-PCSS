4ead35d1c2f0b54e8af32da95c5588a87cf2a062
AApalafox <adrian1144palafoxs@gmail.com>
2021-11-18 15:31:49 +0800

    Revert "added handler function for display users/consultants with ajax endpoints"
    This reverts commit 63b23858ac70a15c5f718b0112959a080b3a5ce0.

--- admin/endpoints/consultant/consultantDisplay.php
+++ admin/endpoints/consultant/consultantDisplay.php
@@ -1,21 +1,22 @@
-
-	<?php
-	
+<?php 
+	//display
+	include("../db.php");
 	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
-	} else {
+	}
+	else{
 		$sql = "SELECT * FROM consultant";
-		$result = $conn->query($sql);
+		$result = $conn -> query($sql);
 
 		if ($result->num_rows > 0) {
-			$consultants = ($result->fetch_all(MYSQLI_ASSOC));
-			// print_r($consultants);
-		} else {
+			echo json_encode($result->fetch_all(MYSQLI_ASSOC));
+		}
+		else{
 			$error = "Error:" . $sql . "<br>" . $conn->error;
-			$response = ["code" => 400, "error" => $error];
+			$response = ["code"=>400, "error" =>$error];
 			echo json_encode($response);
 		}
 	}
-	?>
-	
+
+?>
--- admin/templates/side-nav.php
+++ admin/templates/side-nav.php
@@ -2,14 +2,14 @@
 	<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
 	<a href="dashboard.php"><i class="fas fa-columns fa-1x"></i>Dashboard</a>
 	<a href="consultation-list.php"><i class="fas fa-calendar-alt fa-1x"></i>Consultations</a>
-	<a href="hospital-hours.php"><i class="fas fa-clock fa-1x"></i>Schedule</a>
+	<a href="hospital-hours.php"><i class="fas fa-clock fa-1x"></i>Schedules</a>
 	<a href="notification.php"><i class="fas fa-bell fa-1x"></i>Notifications</a>
 	<a href="user-list.php"><i class="fas fa-users fa-1x"></i>Users</a>
 </div>
 
 <!-- Use any element to open the sidenav -->
 
-<button onclick="openNav()" id="btnMenu" class="btn btnSquare" style="display: none;"><i class="fas fa-bars fa-1x"></i></i></button>
+<button onclick="openNav()" id="btnMenu" class="btn" style="display: none;"><i class="fas fa-bars fa-2x"></i></i></button>
 
 <!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
 <div id="main">
