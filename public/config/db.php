c10bd8de694b41956b4a349a61f8ad6805b3e102
JPysus <72327740+JPysus@users.noreply.github.com>
2021-11-19 19:46:04 +0800

    Delete db.php

--- public/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$database = "pcss";
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $database);
-
-?>
