1f8f67bb471cae978051a17d0e59a5d9548f7ff9
JPysus <72327740+JPysus@users.noreply.github.com>
2021-11-19 19:38:29 +0800

    follow up update
    removed dependencies here
    
    moved to links

--- public/components/login.php
+++ public/components/login.php
@@ -33,11 +33,6 @@
 			</form>
 		</div>
 	</div>
-
-
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js"></script>
 	<script>
 
 	//onsubmit function
