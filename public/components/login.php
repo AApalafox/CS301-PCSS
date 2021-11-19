9618bdaa52fc179f68f91108bdce150f9413a297
JPysus <72327740+JPysus@users.noreply.github.com>
2021-11-19 19:20:07 +0800

    major changes on login.php
    made the html specifically for modal
    
    added script
    
    connects to an endpoints and sets cookie
    
    changed ids for clarity

--- public/components/login.php
+++ public/components/login.php
@@ -1,55 +1,76 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>PCSS Login</title>
-	<?php include '../assets/links.php' ?>
-	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-</head>
-
-<body>
-	<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+	<div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
 		<div class="modal-dialog modal-dialog-centered">
 			<div class="modal-content">
-				<form action="index.php" method="POST">
-					<div class="modal-header justify-content-center">
+				<form id="loginForm" method="POST">
+					<div class="modal-header">
 						<a href="index.php">
 							<h1 class="caption-body fw-bold mt-3">Login</h1>
+							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 						</a>
 					</div>
 					<div class="modal-body">
 						<div class="container">
 							<!-- <h4 class="mb-3 fw-bold  text-center">Login to your account</h4> -->
+							<!-- div for error input-->
+							<div class="alert alert-danger" style="display:none;" id="invalidInput">Invalid Email or Password</div>
 							<label for="email" class="form-label">Email address</label>
 							<div class="input-group flex-nowrap mb-3">
-								<input type="email" class="form-control" placeholder="Email" id="email" name="email" value="" required>
+								<input type="email" class="form-control" placeholder="Email" id="emailLogin" name="email" value="" required>
 								<label class="input-group-text" id="addon-wrapping" for="email"><i class="fas fa-user fa-1x p-2"></i></label>
 							</div>
 							<label for="password" class="form-label">Password</label>
 							<div class="input-group flex-nowrap mb-3">
-								<input type="password" class="form-control" placeholder="Password" id="password" name="password" value="" required>
+								<input type="password" class="form-control" placeholder="Password" id="passwordLogin" name="password" value="" required>
 								<label class="input-group-text" id="addon-wrapping" for="password"><i class="fas fa-lock fa-1x p-2"></i></label>
 							</div>
 						</div>
 					</div>
 					<div class="modal-footer justify-content-between mx-3 my-2">
 						<a href="register.php" class="text-decoration-underline fst-italic">Register new Account</a>
-						<button type="submit" class="btn btnSquare" name="login">Login</button>
+						<button type="submit" class="btn btnSquare" id="loginBtn" name="login">Login</button>
 					</div>
 				</form>
 			</div>
 		</div>
 	</div>
 
-
-</body>
-
-</html>
 <script>
-	$(document).ready(function() {
-		$("#staticBackdrop").modal('show');
+//onsubmit function
+$("#loginForm").bind('submit', function(e){
+	var record = [$("#emailLogin").val(),$("#passwordLogin").val()];
+	 validatePatient(record);
+	if(!isValid){
+		$("#invalidInput").show();
+	}
+	return false;
+});
+//validateInput;
+function validatePatient(record) {
+	var tite= "tite";
+	$.ajax({
+		'url': "endpoints/validatePatient.php",
+		'type': "POST",
+		'data': {
+			'email': record[0],
+			'password': record[1]
+		},
+		success: function(response) {
+
+			response = JSON.parse(response);
+			if (response.code != 400) {
+				for (let row of response){
+					Cookies.set('id', row.patient_id);
+					break;
+				}
+				Cookies.set('type', "patient");
+				window.location.reload();
+			} else {
+				console.log(response.code);
+			}
+		}
+
 	});
+	return false;
+}	
 </script>
