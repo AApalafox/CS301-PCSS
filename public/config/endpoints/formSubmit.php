c80240db3bbf5fa90f41e64de8efb051d0493f32
JPysus <72327740+JPysus@users.noreply.github.com>
2021-11-21 02:46:26 +0800

    also submits a schedule now

--- public/config/endpoints/formSubmit.php
+++ public/config/endpoints/formSubmit.php
@@ -13,7 +13,17 @@
 		$sql = "INSERT INTO `form`(`condition`, `reason`, `status`, `form_dateTime`, `patient_id`) 
 		VALUES ('$condition','$reason','pending','$form_dateTime','$patient_id');";
 		if ($conn -> query($sql) === TRUE) {
-			$response = ["code"=>200];
+			$last_id = $conn->insert_id;
+			$sql = "INSERT INTO `schedule`(`schedule_dateTime`, `status`, `form_id`) 
+				VALUES ('$form_dateTime', 'Pending','$last_id');";
+
+			if ($conn -> query($sql) === TRUE) {
+				$response = ["code"=>200];
+			}
+			else{
+				$error = "Error:" . $sql . "<br>" . $conn->error;
+				$response = ["code"=>400, "error" =>$error];
+			}
 		}
 		else{
 			$error = "Error:" . $sql . "<br>" . $conn->error;
