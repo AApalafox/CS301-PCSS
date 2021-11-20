82e24953bac109bf4cf6217ee2a47ea12af0af13
JPysus <72327740+JPysus@users.noreply.github.com>
2021-11-20 10:55:00 +0800

    Add files via upload

---
+++ public/config/endpoints/formSubmit.php
@@ -0,0 +1,25 @@
+<?php 
+	include("db.php");
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	else{
+		$condition = $_POST['condition'];
+		$reason = $_POST['reason'];
+		$form_dateTime = $_POST['form_dateTime'];
+		$patient_id = $_POST['patient_id'];
+
+		$sql = "INSERT INTO `form`(`condition`, `reason`, `status`, `form_dateTime`, `patient_id`) 
+		VALUES ('$condition','$reason','pending','$form_dateTime','$patient_id');";
+		if ($conn -> query($sql) === TRUE) {
+			$response = ["code"=>200];
+		}
+		else{
+			$error = "Error:" . $sql . "<br>" . $conn->error;
+			$response = ["code"=>400, "error" =>$error];
+		}
+		echo json_encode($response);
+	}
+
+?>
