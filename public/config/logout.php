ee51c28144fa4bc1aed0e6f4b06696ba6c01a9c0
JPysus <72327740+JPysus@users.noreply.github.com>
2021-11-20 23:58:45 +0800

    logout cookies on public

--- public/config/logout.php
+++ public/config/logout.php
@@ -1,12 +1,8 @@
 <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js"></script>
 <script>
 	//logout.php
-	if(Cookies.get('id'))
-		Cookies.remove('id');
-	if(Cookies.get('type'))
-		Cookies.remove('type');
-	if(Cookies.get('name'))
-		Cookies.remove('name');
-	
+	Cookies.remove('id',{path:'/pcss/public'});
+	Cookies.remove('type',{path: '/pcss/public'});
+	Cookies.remove('name',{path: '/pcss/public'});
 	window.location.replace("../index.php");
 </script>
