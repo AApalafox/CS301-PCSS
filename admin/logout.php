7fbe9e5fc5eab9c07783339773c1a9c6089748a2
JPysus <72327740+JPysus@users.noreply.github.com>
2021-11-20 23:06:58 +0800

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -1,7 +1,12 @@
 <?php
 //logout.php
-setcookie("id", "", time()-3600);
+if(isset($_COOKIES['id']))
+	setcookie("id", "", time()-3600);
+
+if(isset($_COOKIES['type']))
 setcookie("type", "", time()-3600);
+
+if(isset($_COOKIES['name']))
 setcookie("name", "", time()-3600);
 
 header("location:index.php");
