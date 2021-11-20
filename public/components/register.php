9b6570cd3ead15d4f961f65d3e2e34da88d74e9b
JPysus <72327740+JPysus@users.noreply.github.com>
2021-11-20 12:40:13 +0800

    connected to database

--- public/components/register.php
+++ public/components/register.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<?php
+	$ajaxVar = ["fname","lname","birthdate","email", "password", "phone"];
+?>
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,51 +13,49 @@
 </head>
 
 <body>
-	<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+	<div class="modal fade" id="modalPatientRegister" data-bs-backdrop="static" data-bs-keyboard="false">
 		<div class="modal-dialog modal-dialog-centered">
 			<div class="modal-content">
-				<form action="index.php" method="POST" class="needs-validation" novalidate>
+				<form id="patientRegister" method="POST">
 					<div class="modal-header justify-content-center">
-						<a href="index.php">
-							<h1 class="caption-body fw-bold mt-3">Register</h1>
-						</a>
+						<h1 class="caption-body fw-bold mt-3">Register</h1>
 					</div>
 					<div class="modal-body">
 						<div class="container">
 							<div class="row">
 								<div class="col">
-									<label for="validationCustom01" class="form-label">First name</label>
-									<input type="text" class="form-control" id="fname" name="fname" value="Mark" required>
+									<label class="form-label">First name</label>
+									<input type="text" class="form-control" placeholder="First Name" id="<?=$ajaxVar[0]?>" name="<?=$ajaxVar[0]?>" value="" required>
 								</div>
 								<div class="col">
-									<label for="validationCustom02" class="form-label">Last name</label>
-									<input type="text" class="form-control" id="lname" name="lname" value="Otto" required>
+									<label class="form-label">Last name</label>
+									<input type="text" class="form-control" placeholder="Last Name" id="<?=$ajaxVar[1]?>" name="<?=$ajaxVar[1]?>" value="" required>
 								</div>
 							</div>
 							<div class="row">
 								<div class="col">
-									<label for="validationCustom01" class="form-label">Birthday</label>
-									<input type="text" class="form-control" id="bday" name="bday" value="Mark" required>
+									<label class="form-label">Birthday</label>
+									<input type="text" class="form-control" placeholder="Birthday" id="<?=$ajaxVar[2]?>" name="<?=$ajaxVar[2]?>" value="" required readonly>
 								</div>
 								<div class="col">
-									<label for="validationCustom02" class="form-label">Phone</label>
-									<input type="text" class="form-control" id="phone" name="phone" value="Otto" required>
+									<label class="form-label">Phone</label>
+									<input type="text" class="form-control" placeholder="Phone #" id="<?=$ajaxVar[5]?>" name="<?=$ajaxVar[5]?>" value="" required>
 								</div>
 							</div>
 							<label for="email" class="form-label">Email address</label>
 							<div class="input-group flex-nowrap mb-1">
-								<input type="email" class="form-control" placeholder="Email" id="email" name="email" value="" required>
+								<input type="email" class="form-control" placeholder="Email" id="<?=$ajaxVar[3]?>" name="<?=$ajaxVar[3]?>" value="" required>
 								<label class="input-group-text" id="addon-wrapping" for="email"><i class="fas fa-user p-2"></i></label>
 							</div>
 							<label for="password" class="form-label">Password</label>
 							<div class="input-group flex-nowrap mb-1">
-								<input type="password" class="form-control" placeholder="Password" id="password" name="password" value="" required>
+								<input type="password" class="form-control" placeholder="Password" id="<?=$ajaxVar[4]?>" name="<?=$ajaxVar[4]?>" value="" required>
 								<label class="input-group-text" id="addon-wrapping" for="password"><i class="fas fa-lock  p-2"></i></label>
 							</div>
 						</div>
 					</div>
 					<div class="modal-footer justify-content-between mx-3 my-2">
-						<a href="login.php" class="text-decoration-underline fst-italic">Login to my account</a>
+						<a href="../index.php" class="text-decoration-underline fst-italic">Login to my account</a>
 						<button type="submit" class="btn btnSquare" name="login">Login</button>
 					</div>
 				</form>
@@ -69,6 +69,51 @@
 </html>
 <script>
 	$(document).ready(function() {
-		$("#staticBackdrop").modal('show');
+		$("#modalPatientRegister").modal('show');
 	});
+	//birthday datetimepicker
+	$("#<?=$ajaxVar[2]?>").datetimepicker({
+		format: 'yyyy-mm-dd',
+		minView:2,
+		autoclose: true
+	});
+	//onsubmit function
+	$('#patientRegister').bind('submit',function(){
+		record = [];
+		<?php foreach ($ajaxVar as $row): ?>
+			record.push($("#<?=$row?>").val());
+		<?php endforeach ?>
+		//console.log(record);
+		insertPatient(record);
+		return false;
+	});
+	
+	//INSERT
+	function insertPatient(record){
+		$.ajax({
+			'url':"../config/endpoints/patientRegister.php",
+			'type':"POST",
+			'data':{
+				<?php 
+					for($i = 0; $i <count($ajaxVar); $i++)
+						echo '',$ajaxVar[$i],':record[',$i,'],';
+				?>
+			},
+			success: function(response){
+				response = JSON.parse(response);
+				if(response.code == 200){
+					console.log(response.code);
+					alert("Successfully submitted a schedule.");
+					window.location.replace("../index.php");
+				}
+				else if (response.code == 400){
+					console.log(response.error);
+
+				}
+				else{
+					console.log(response.code);
+				}
+			}
+		});
+	}
 </script>
