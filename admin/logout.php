d8dc1364948792b164a51fddc3d5da481efaf3fc
JPysus <72327740+JPysus@users.noreply.github.com>
2021-11-21 00:08:52 +0800

    fixed not logging out

--- admin/logout.php
+++ admin/logout.php
@@ -1,12 +1,7 @@
 <?php
 //logout.php
-if(isset($_COOKIES['id']))
-	setcookie("id", "", time()-3600);
-
-if(isset($_COOKIES['type']))
+setcookie("id", "", time()-3600);
 setcookie("type", "", time()-3600);
-
-if(isset($_COOKIES['name']))
 setcookie("name", "", time()-3600);
 
 header("location:index.php");
