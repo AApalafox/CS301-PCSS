a42372cf3410a4358bb225cf1194c8d8acb9a380
JPysus <72327740+JPysus@users.noreply.github.com>
2021-11-20 23:06:24 +0800

    also adds deletes name now

--- public/config/logout.php
+++ public/config/logout.php
@@ -1,9 +1,12 @@
 <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js"></script>
 <script>
 	//logout.php
-	if(Cookies.get('id')||Cookies.get('type')){
+	if(Cookies.get('id'))
 		Cookies.remove('id');
+	if(Cookies.get('type'))
 		Cookies.remove('type');
-	}
+	if(Cookies.get('name'))
+		Cookies.remove('name');
+	
 	window.location.replace("../index.php");
 </script>
