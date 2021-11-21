197a88032db38640662f1bed260ff0404f3fc454
AApalafox <adrian1144palafoxs@gmail.com>
2021-11-21 13:58:48 +0800

    show hospital_schedule on public

--- public/components/schedule.php
+++ public/components/schedule.php
@@ -1,3 +1,27 @@
+<?php
+include("config/endpoints/db.php");
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+} else {
+	$days = "days";
+	$time = "time";
+	$sql = "SELECT value FROM hospital_schedule WHERE field_name IN ('$days','$time')";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		$hospital_schedule = ($result->fetch_all(MYSQLI_ASSOC));
+		$hospital_days = explode(",", ($hospital_schedule[0]['value']));
+		$hospital_days = $hospital_days[0] . "-" . end($hospital_days);
+		$hospital_time = $hospital_schedule[1];
+		$hospital_time = $hospital_time['value'];
+	} else {
+		$error = "Error:" . $sql . "<br>" . $conn->error;
+		$response = ["code" => 400, "error" => $error];
+		echo json_encode($response);
+	}
+}
+?>
 
 <div class="row mb-5">
 
@@ -22,21 +46,21 @@
 		</div>
 		<ul class="list-group list-group-flush mb-3">
 			<a href="#">
-			<li class="list-group-item">
+				<li class="list-group-item">
 					<div class="row">
-						<div class="col">Mon-Fri</div>
-						<div class="col text-end">8:00AM - 10:00PM</div>
+						<div class="col" id="daysSchedule"> <?php echo "$hospital_days" ?> </div>
+						<div class="col text-end" id="timeSchedule"> <?php echo "$hospital_time" ?> </div>
 					</div>
 				</li>
 			</a>
-			<a href="#">
+			<!-- <a href="#">
 				<li class="list-group-item">
 					<div class="row">
 						<div class="col">Sat-Sun</div>
 						<div class="col text-end">8:00AM - 8:00PM</div>
 					</div>
 				</li>
-			</a>
+			</a> -->
 		</ul>
 	</div>
 
